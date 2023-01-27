0d3033822dc069726490466e1ee2be9ea0414079
AgustinLaValla <agulavalla@gmail.com>
2023-01-27 14:57:11 -0300

    Set background sync for put and delete request

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -43,3 +43,15 @@ registerRoute(
   new NetworkOnly({ plugins: [bgSyncPlugin]}),
   'POST'
 )
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({ plugins: [bgSyncPlugin]}),
+  'PUT'
+)
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({ plugins: [bgSyncPlugin]}),
+  'DELETE'
+)
