37d8e25cfd17a0d5d6f766f002298970a342d8df
AgustinLaValla <agulavalla@gmail.com>
2023-01-28 13:48:20 -0300

    Add online-offline status message

--- react-calendar-frontend/src/components/ui/Navbar.js
+++ react-calendar-frontend/src/components/ui/Navbar.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { startLogout } from '../../actions/auth';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { startLogout } from "../../actions/auth";
+import { Detector } from "react-detect-offline";
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
 
-    const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
 
-    const handleLogout = () => {
-        dispatch( startLogout() );
-    }
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">{name}</span>
 
-    return (
-        <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">
-                { name }
+      <span className="navbar-brand">
+        <Detector
+          render={({ online }) => (
+            <span className={`navbar-brand ${online ? "text-light" : "text-danger"}`}>
+               {online ? "Online" : "Offline"}
             </span>
-            
-            <button 
-                className="btn btn-outline-danger"
-                onClick={ handleLogout }
-            >
-                <i className="fas fa-sign-out-alt"></i>
-                <span> Exit</span>
-            </button>
+          )}
+        />
+      </span>
 
-        </div>
-    )
-}
+      <button className="btn btn-outline-danger" onClick={handleLogout}>
+        <i className="fas fa-sign-out-alt"></i>
+        <span> Exit</span>
+      </button>
+    </div>
+  );
+};
